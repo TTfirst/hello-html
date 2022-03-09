cf63e5216cdededf8f3f2f5a04bbb91667d7d482
TTfirst <continue25@163.com>
2022-03-09 09:34:04 +0800

    new

---
+++ codepen/js-2/index.js
@@ -0,0 +1,6 @@
+var hide_nav = document.querySelector(".hide_nav");
+/* var Width_ = document.body.clientWidth();
+if (Width_ < 400 + "px") {
+    hide_nav.style.display = "none";
+} */
+/* console.log(document.body.clientWidth); */
